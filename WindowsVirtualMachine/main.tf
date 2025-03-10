402aa51fb4db6c8ff6098cc666a7a59a0764ac03
Huseyin <955481@rotterdam.nl>
2025-03-10 08:28:53 +0100

    Add files via upload

---
+++ WindowsVirtualMachine/main.tf
@@ -0,0 +1,294 @@
+resource "azurerm_network_security_group" "example" {
+  name                = var.NSG_name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  tags = merge(var.extra_tags, { source = "Terraform" })
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.vm_name}-nic"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "${var.vm_name}-nic-ipconfig"
+    subnet_id                     = data.azurerm_subnet.snet.id
+    private_ip_address_allocation = var.private_ip_address_allocation
+  }
+
+  tags = merge(var.extra_tags, { source = "Terraform" })
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+  
+  depends_on = [azurerm_network_security_group.example]
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.example.id
+  depends_on                = [azurerm_network_interface.main]
+}                                               
+
+resource "azurerm_windows_virtual_machine" "main" {
+  name                       = var.vm_name
+  location                   = data.azurerm_resource_group.rg.location
+  resource_group_name        = data.azurerm_resource_group.rg.name
+  size                       = var.vm_size
+  admin_username             = var.admin_username
+  admin_password             = var.admin_password
+  network_interface_ids      = [azurerm_network_interface.main.id]
+  license_type               = var.license_type
+  secure_boot_enabled        = var.secure_boot_enabled
+  vtpm_enabled               = var.vtpm_enabled
+  enable_automatic_updates   = var.enable_automatic_updates
+  patch_mode                 = var.patch_mode
+  encryption_at_host_enabled = var.encryption_at_host_enabled
+  computer_name              = var.vm_name
+  patch_assessment_mode      = var.patch_assessment_mode
+  source_image_id            = var.source_image_id
+
+  os_disk {
+    caching                = var.caching_os_disk
+    storage_account_type   = var.storage_account_type_os_disk
+    disk_size_gb           = var.disk_size_os_disk 
+    disk_encryption_set_id = var.disk_encryption_set_id
+  }
+
+  lifecycle {
+    ignore_changes = [identity, tags]
+  }
+
+  tags = merge(var.extra_tags, { "update schedule" = var.tag_update_schedule, source = "Terraform" })
+
+
+  depends_on = [azurerm_network_interface_security_group_association.example]
+}
+
+resource "azurerm_managed_disk" "example" {
+  name                          = var.data_disk_name
+  location                      = data.azurerm_resource_group.rg.location
+  resource_group_name           = data.azurerm_resource_group.rg.name
+  storage_account_type          = var.storage_account_type_data_disk
+  create_option                 = var.managed_disk_create_option
+  disk_size_gb                  = var.managed_disk_size
+  disk_encryption_set_id        = var.disk_encryption_set_id
+  
+  tags = merge(var.extra_tags, { source = "Terraform" })
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+
+  depends_on = [azurerm_windows_virtual_machine.main]
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "example" {
+  managed_disk_id    = azurerm_managed_disk.example.id
+  virtual_machine_id = azurerm_windows_virtual_machine.main.id
+  lun                = var.lun_data_disk
+  caching            = var.caching_data_disk
+  depends_on         = [azurerm_managed_disk.example]
+}
+
+# resource "azurerm_virtual_machine_extension" "disk_setup" {
+#   name                      = "assign-drive-letter"
+#   virtual_machine_id        = azurerm_windows_virtual_machine.main.id
+#   publisher                 = "Microsoft.Compute"
+#   type                      = "CustomScriptExtension"
+#   type_handler_version      = "1.10"
+#   depends_on                = [azurerm_virtual_machine_data_disk_attachment.example]
+
+#   lifecycle {
+#     ignore_changes = [tags]
+#   }
+
+#   settings = <<EOT
+#   {
+#   "commandToExecute": "powershell -ExecutionPolicy Bypass -Command \"$disk = Get-Disk | Where-Object PartitionStyle -eq 'RAW'; if ($disk) { Initialize-Disk -Number $disk.Number -PartitionStyle GPT -Confirm:$false; $partition = New-Partition -DiskNumber $disk.Number -UseMaximumSize -AssignDriveLetter; Format-Volume -DriveLetter $partition.DriveLetter -FileSystem NTFS -Confirm:$false } else { Write-Output 'No RAW disk found for initialization.' }\""
+#   }
+#   EOT
+# }
+
+resource "azurerm_virtual_machine_extension" "disk_setup" {
+  name                      = "assign-drive-letter"
+  virtual_machine_id        = azurerm_windows_virtual_machine.main.id
+  publisher                 = "Microsoft.Compute"
+  type                      = "CustomScriptExtension"
+  type_handler_version      = "1.10"
+  depends_on                = [azurerm_virtual_machine_data_disk_attachment.example]
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+
+  settings = <<EOT
+  {
+    "commandToExecute": "powershell -ExecutionPolicy Bypass -Command \"$cdDrive = Get-WmiObject -Query 'SELECT * FROM Win32_Volume WHERE DriveType = 5 AND DriveLetter = \\\"D:\\\"'; if ($cdDrive) { $cdDrive.DriveLetter = \\\"Z:\\\"; $cdDrive.Put(); }; $disk = Get-Disk | Where-Object PartitionStyle -eq 'RAW'; if ($disk) { Initialize-Disk -Number $disk.Number -PartitionStyle GPT -Confirm:$false; $partition = New-Partition -DiskNumber $disk.Number -UseMaximumSize -AssignDriveLetter; Format-Volume -DriveLetter $partition.DriveLetter -FileSystem NTFS -Confirm:$false } else { Write-Output 'No RAW disk found for initialization.' }\""
+  }
+  EOT
+}
+
+
+
+# Resource for setting DNS Suffix
+resource "azurerm_virtual_machine_run_command" "set_dns_suffix" {
+  name               = "SetDNSSuffix"
+  virtual_machine_id = azurerm_windows_virtual_machine.main.id
+  location           = azurerm_windows_virtual_machine.main.location
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+  
+  source {
+    script = <<EOF
+     Set-DnsClient -InterfaceIndex (Get-NetAdapter -InterfaceDescription "Microsoft Hyper-V*").IfIndex -ConnectionSpecificSuffix "rotterdam.local" -UseSuffixWhenRegistering $true; Register-DnsClient
+     EOF
+  }
+  depends_on = [azurerm_virtual_machine_extension.disk_setup]
+}
+
+# Resource for Azure Policy for Windows extension
+resource "azurerm_virtual_machine_extension" "azure_policy_windows" {
+  name                 = "AzurePolicyforWindows"
+  virtual_machine_id    = azurerm_windows_virtual_machine.main.id
+  publisher             = "Microsoft.GuestConfiguration"
+  type                  = "ConfigurationforWindows"
+  type_handler_version = "1.29"
+  auto_upgrade_minor_version = true
+  automatic_upgrade_enabled = true
+  depends_on = [azurerm_virtual_machine_run_command.set_dns_suffix]
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+# Resource for Monitoring Dependency Agent extension
+resource "azurerm_virtual_machine_extension" "monitoring_dependency_agent" {
+  name                 = "Microsoft.Azure.Monitoring.DependencyAgent"
+  virtual_machine_id    = azurerm_windows_virtual_machine.main.id
+  publisher             = "Microsoft.Azure.Monitoring.DependencyAgent"
+  type                  = "DependencyAgentWindows"
+  type_handler_version = "9.10"
+  auto_upgrade_minor_version = true
+  automatic_upgrade_enabled = true
+  depends_on = [azurerm_virtual_machine_extension.azure_policy_windows]
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+# Resource for Microsoft Defender Agent Extension
+resource "azurerm_virtual_machine_extension" "mde_windows" {
+  name                       = "MDE.Windows"
+  virtual_machine_id         = azurerm_windows_virtual_machine.main.id
+  publisher                  = "Microsoft.Azure.AzureDefenderForServers"
+  type                       = "MDE.Windows"
+  type_handler_version       = "1.0"
+  auto_upgrade_minor_version = true
+
+  lifecycle {
+    ignore_changes = [tags, settings]
+  }
+
+  settings                   = jsonencode({
+    autoUpdate        = true
+    azureResourceId   = azurerm_windows_virtual_machine.main.id
+    forceReOnboarding = false
+    vNextEnabled      = true
+  })
+
+  depends_on                 = [azurerm_virtual_machine_extension.monitoring_dependency_agent]
+}
+
+# Resource for Azure Monitor Windows Agent extension
+resource "azurerm_virtual_machine_extension" "azure_monitor_windows_agent" {
+  name                 = "AzureMonitorWindowsAgent"
+  virtual_machine_id    = azurerm_windows_virtual_machine.main.id
+  publisher             = "Microsoft.Azure.Monitor"
+  type                  = "AzureMonitorWindowsAgent"
+  type_handler_version = "1.0"
+  auto_upgrade_minor_version = true
+  automatic_upgrade_enabled = true
+  depends_on = [azurerm_virtual_machine_extension.mde_windows]
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+# Resource to join domain
+resource "azurerm_virtual_machine_extension" "domain_join" {
+  name                 = "domainJoin"
+  virtual_machine_id    = azurerm_windows_virtual_machine.main.id
+  publisher             = "Microsoft.Compute"
+  type                  = "JsonADDomainExtension"
+  type_handler_version = "1.3"
+  auto_upgrade_minor_version = true
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+
+  settings = jsonencode({
+    Name        = "rotterdam.local"
+    User        = "rotterdam\\SA_SRV_TERRAFORM_DJ"
+    Restart     = "true"
+    Options     = 3
+    OUPath      = var.OU_path
+  })
+
+  protected_settings = jsonencode({
+    Password = "ufpIBoYRXUNjkFo<>"
+  })
+  depends_on = [azurerm_virtual_machine_extension.azure_monitor_windows_agent]
+}
+
+resource "azurerm_virtual_machine_run_command" "set_timezone_vm" {
+  name               = "SetTimeZoneOnVm"
+  virtual_machine_id = azurerm_windows_virtual_machine.main.id
+  location           = azurerm_windows_virtual_machine.main.location
+  source {
+  # Command to set timezone on vm
+  script = <<EOF
+    Set-TimeZone -Id "W. Europe Standard Time"
+  EOF
+  }
+  depends_on = [azurerm_virtual_machine_extension.domain_join]
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_virtual_machine_run_command" "restart_vm" {
+  name               = "RestartVM"
+  virtual_machine_id = azurerm_windows_virtual_machine.main.id
+  location           = azurerm_windows_virtual_machine.main.location
+  source {
+  # Command to restart the Windows VM
+  script = <<EOF
+    Restart-Computer -Force
+  EOF
+  }
+  depends_on = [azurerm_virtual_machine_run_command.set_timezone_vm]
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+
+
+
+
+
---
+++ WindowsVirtualMachine/variables.tf
@@ -0,0 +1,196 @@
+variable "resource_group_name" {
+  description = "The name of the existing resource group in which to create the resource"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Specifies the name of the Subnet."
+  type        = string
+}
+
+variable "subnet_rg" {
+  description = "Specifies the name of the resource group the Virtual Network is located in."
+  type        = string
+}
+
+variable "subnet_vnet" {
+  description = "Specifies the name of the Virtual Network this Subnet is located within."
+  type        = string
+}
+
+variable "NSG_name" {
+  description = "Specifies the name of the network security group. Changing this forces a new resource to be created."
+  type        = string 
+}
+
+variable "private_ip_address_allocation" {
+  description = "The allocation method used for the Private IP Address. Possible values are Dynamic and Static."
+  type        = string
+  default     = "Dynamic"
+}
+
+variable "vm_name" {
+  description = "The name of the Windows Virtual Machine. Changing this forces a new resource to be created."
+  type        = string
+}
+
+variable "vm_size" {
+  description = "The SKU which should be used for this Virtual Machine"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "The username of the local administrator used for the Virtual Machine. Changing this forces a new resource to be created."
+  type        = string 
+}
+
+variable "admin_password" {
+  description = "The Password which should be used for the local-administrator on this Virtual Machine. Changing this forces a new resource to be created."
+  type        = string 
+}
+
+variable "license_type" {
+  description = "Specifies the type of on-premise license (also known as Azure Hybrid Use Benefit) which should be used for this Virtual Machine. Possible values are None, Windows_Client and Windows_Server."
+  type        = string
+  default     = "Windows_Server"
+}
+
+variable "secure_boot_enabled" {
+  description = "Specifies if Secure Boot and Trusted Launch is enabled for the Virtual Machine. Changing this forces a new resource to be created."
+  type        = bool
+  default     = true
+}
+
+variable "vtpm_enabled" {
+  description = "Specifies if vTPM (virtual Trusted Platform Module) and Trusted Launch is enabled for the Virtual Machine. Changing this forces a new resource to be created."
+  type        = bool
+  default     = true
+}
+
+variable "enable_automatic_updates" {
+  description = "Specifies if Automatic Updates are Enabled for the Windows Virtual Machine. Changing this forces a new resource to be created."
+  type        = bool
+  default     = false
+}
+
+variable "patch_mode" {
+  description = "Specifies the mode of in-guest patching to this Windows Virtual Machine. Possible values are Manual, AutomaticByOS and AutomaticByPlatform."
+  type        = string
+  default     = "Manual"
+}
+
+variable "encryption_at_host_enabled" {
+  description = "Should all of the disks (including the temp disk) attached to this Virtual Machine be encrypted by enabling Encryption at Host"
+  type        = bool
+  default     = true
+}
+
+variable "patch_assessment_mode" {
+  description = "Specifies the mode of VM Guest Patching for the Virtual Machine. Possible values are AutomaticByPlatform or ImageDefault."
+  type        = string
+  default     = "AutomaticByPlatform"
+}
+
+variable "source_image_id" {
+  description = "The ID of the Image which this Virtual Machine should be created from. Changing this forces a new resource to be created."
+  type        = string
+}
+
+variable "caching_os_disk" {
+  description = "The Type of Caching which should be used for the Internal OS Disk. Possible values are None, ReadOnly and ReadWrite."
+  type        = string
+  default     = "ReadWrite"
+}
+
+variable "storage_account_type_os_disk" {
+  description = "The Type of Storage Account which should back this the Internal OS Disk. Possible values are Standard_LRS, StandardSSD_LRS, Premium_LRS, StandardSSD_ZRS and Premium_ZRS. Changing this forces a new resource to be created."
+  type        = string
+  default     = "Premium_LRS"
+}
+
+variable "disk_size_os_disk" {
+  description = "The Size of the Internal OS Disk in GB, if you wish to vary from the size used in the image this Virtual Machine is sourced from."
+  type        = number
+  default     = 256 
+}
+
+variable "disk_encryption_set_id" {
+  description = "The ID of the Disk Encryption Set which should be used to Encrypt the OS or managed Disk."
+  type        = string 
+}
+
+variable "data_disk_name" {
+  description = "Specifies the name of the Managed Disk. Changing this forces a new resource to be created."
+  type        = string
+}
+
+variable "storage_account_type_data_disk" {
+  description = "The type of storage to use for the managed disk."
+  type        = string
+  default     = "Premium_LRS"
+}
+
+variable "managed_disk_create_option" {
+  description = "The method to use when creating the managed disk. Changing this forces a new resource to be created."
+  type        = string
+  default     = "Empty"
+}
+
+variable "managed_disk_size" {
+  description = "Specifies the size of the managed disk to create in gigabytes."
+  type        = number
+  default     = 32
+}
+
+variable "lun_data_disk" {
+  description = "The Logical Unit Number of the Data Disk, which needs to be unique within the Virtual Machine. Changing this forces a new resource to be created."
+  type        = number
+  default     = 0
+}
+
+variable "caching_data_disk" {
+  description = "Specifies the caching requirements for this Data Disk. Possible values include None, ReadOnly and ReadWrite."
+  type        = string
+  default     = "ReadWrite"
+}
+
+variable "OU_path" {
+  description = "The Organizational Unit (OU) path for the server"
+  type        = string
+  //default     = "OU=Management servers,OU=Servers,DC=rotterdam,DC=local"
+
+  validation {
+    condition     = can(regex("^OU=.*DC=.*$", var.OU_path))
+    error_message = "The OU Path must start with 'OU=' and contain 'DC=' components."
+  }
+}
+
+variable "extra_tags" {
+  description = "Set the change number as a tag"
+  type        = map(string)
+}
+
+variable "tag_update_schedule" {
+  description = "Map of tags including update schedule."
+  type        = map(string)
+
+  validation {
+    condition = contains([
+      "dinsdag 23:00", 
+      "donderdag 00:00", 
+      "maandag 23:00", 
+      "vrijdag 01:00", 
+      "woensdag 00:00", 
+      "handmatig"
+    ], var.tags["update schedule"])
+    error_message = "Invalid update schedule. Allowed values: dinsdag 23:00, donderdag 00:00, maandag 23:00, vrijdag 01:00, woensdag 00:00, handmatig."
+  }
+}
+
+
+
+
+
+
+
+

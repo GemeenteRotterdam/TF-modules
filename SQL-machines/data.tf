df3051b5152fdead7c3d9eaa6d135282743b143e
Huseyin <955481@rotterdam.nl>
2025-04-03 14:25:53 +0200

    Update data.tf

--- SQL-machines/data.tf
+++ SQL-machines/data.tf
@@ -2,7 +2,7 @@ data "azurerm_resource_group" "resource_group" {
   name = var.resource_group_name
 }
 
-data "azurerm_subnet" "snet" {
+data "azurerm_subnet" "example" {
   name                 = var.subnet_name
   resource_group_name  = var.subnet_rg
   virtual_network_name = var.subnet_vnet

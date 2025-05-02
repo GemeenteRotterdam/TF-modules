942062c81db1ae95119de5db513f611ea1f04c3c
Huseyin <955481@rotterdam.nl>
2025-05-02 14:42:54 +0200

    Update main.tf

--- WindowsVirtualMachine/main.tf
+++ WindowsVirtualMachine/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_security_group" "example" {
-  name                = var.nsg_name
+  name                = "${var.vm_name}-nsg"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
 

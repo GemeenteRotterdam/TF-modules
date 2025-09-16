3d46d32b0334ffbd24ba502013bce6f14a7c22a9
danny-keizer <fc122821@hotmail.com>
2025-09-16 09:01:27 +0200

    Fix: Refactor

--- VirtualNetwork/main.tf
+++ VirtualNetwork/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "this" {
-  name                           = var.resource_group_name
+  name                           = var.virtual_network_name
   address_space                  = var.address_space
   dns_servers                    = var.dns_servers
   resource_group_name            = data.azurerm_resource_group.rg.name

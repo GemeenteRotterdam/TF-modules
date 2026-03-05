b055d66d567b75910907052cc6315a6a69924e84
Huseyin <955481@rotterdam.nl>
2026-03-05 11:52:32 +0100

    Update main.tf

--- VirtualNetwork/main.tf
+++ VirtualNetwork/main.tf
@@ -9,13 +9,15 @@ resource "azurerm_virtual_network" "this" {
 
 resource "azurerm_subnet" "this" {
   for_each                                      = var.subnets
-  name                                          = each.key
+  name                                          = each.key //dynamisch
   virtual_network_name                          = azurerm_virtual_network.this.name
   resource_group_name                           = var.resource_group_name
-  address_prefixes                              = each.value.address_space
+  address_prefixes                              = each.value.address_space // dynamisch
   service_endpoint_policy_ids                   = each.value.service_endpoint_policy_ids
   service_endpoints                             = each.value.service_endpoints
   default_outbound_access_enabled               = false
   private_link_service_network_policies_enabled = true
   private_endpoint_network_policies             = "Enabled"
+// delegation toevoegen, alleen als waarde niet null is
+
 }
